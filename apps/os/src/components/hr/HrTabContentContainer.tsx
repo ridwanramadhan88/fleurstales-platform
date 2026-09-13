228d415e868d69cb9e05ea38b0fcd601ab99b7dc
ridwanramadhan88 <ridwanramadhan88@gmail.com>
2026-09-13 12:12:58 +0700

    Enable HR managed account editing

--- apps/os/src/components/hr/HrTabContentContainer.tsx
+++ apps/os/src/components/hr/HrTabContentContainer.tsx
@@ -1,9 +1,15 @@
 import type { FC } from 'react'
+import { useUserStore } from '../../store/userStore'
+import { installHrManagedEmployeeAccessMutation } from '../../store/hrManagedEmployeeAccessMutation'
 import { HrTabContent, type HrTabContentProps } from './HrTabContent'
 import { useHrTabContentController } from './HrTabContentController'
 
 export const HrTabContentContainer: FC<HrTabContentProps> = (props) => {
-  const viewModel = useHrTabContentController(props)
+  const role = useUserStore((state) => state.role)
+  if (role === 'hr') installHrManagedEmployeeAccessMutation()
 
-  return <HrTabContent {...viewModel} />
+  const viewModel = useHrTabContentController(props)
+  const canEditCredentials = viewModel.canEditCredentials || (role === 'hr' && viewModel.canManageEmployeeDetails)
+
+  return <HrTabContent {...viewModel} canEditCredentials={canEditCredentials} />
 }
